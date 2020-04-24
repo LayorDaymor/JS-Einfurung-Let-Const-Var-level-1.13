2925f23b4921736d6381c612aebdcea1f3e6ce37
SuperCode <supercode@SuperCodes-MacBook-Air.local>
2020-04-24 23:54:32 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+var addition_operator = 1 ;
+var addition=1
+addition_operator +=addition;
+console.log(addition_operator);
+
+var subtraktion_operator = 2;
+var subtraktion=1
+subtraktion_operator -=subtraktion;
+console.log(subtraktion_operator);
+
+var multiplication_operator = 2;
+var multiplication=2
+multiplication_operator *=multiplication
+console.log( multiplication_operator);
+
+var division_operator = 4;
+var division=2
+division_operator /=division;
+console.log(division_operator);
+
+var modulus_operator = 14;
+var modulus=5
+modulus_operator%=modulus
+console.log(modulus_operator);
